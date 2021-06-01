9507c19252a95d5bb9187eda6f293a2f9e97cedf
Farshid Mehrtash <mehrtash.fm@live.com>
2021-06-02 00:02:35 +0430

    Spring boot - first

---
+++ back/firstsession/src/main/java/com/simplilearn/firstsession/model/Book.java
@@ -0,0 +1,55 @@
+package com.simplilearn.firstsession.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "tbl_book")
+public class Book {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+	
+	private String author;
+
+	public Book(String title, String author) {
+		super();
+		this.title = title;
+		this.author = author;
+	}
+
+	public Book() {
+		super();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ back/firstsession/src/test/java/com/simplilearn/firstsession/FirstsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.firstsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
